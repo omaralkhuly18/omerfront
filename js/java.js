dde61b88ad856a444b25608fe622a77b46e2e96f
omaralkhuly18 <omaralkhuly18@gmail.com>
2023-10-08 17:49:59 +0300

    edit front

--- js/java.js
+++ js/java.js
@@ -234,21 +234,7 @@ function closeAllSelect(elmnt) {
 document.addEventListener("click", closeAllSelect);
 // select especially downlist
 
-// jquery a wasted
-var $ = jQuery;
-jQuery(document).ready(function ($) {
-  jQuery("#carouselExampleControlsNoTouching").carousel();
 
-  jQuery(".ccarousel-control-next").click(function () {
-    jQuery("#carouselExampleControlsNoTouching").carousel("next");
-  });
-
-  jQuery(".carousel-control-prev").click(function () {
-    jQuery("#carouselExampleControlsNoTouching").carousel("prev");
-  });
-
-});
-// jquery a wasted
 
 // search from hayper cars 
 function myFunction() {
@@ -280,3 +266,141 @@ function myFunction() {
 
 }
 // search from hayper cars 
+
+// Image Comparison Slider
+function initComparisons() {
+  var imageTow, movent;
+  imageTow = document.getElementsByClassName("img-comp-overlay");
+  for (movent = 0; movent < imageTow.length; movent++) {
+
+    compareImages(imageTow[movent]);
+  }
+
+  function compareImages(img) {
+
+    var slider, img, clicked = 0, moventFirst, moventSecond;
+
+    moventFirst = img.offsetWidth;
+    moventSecond = img.offsetHeight;
+    img.style.width = (moventFirst / 2) + "px";
+
+    slider = document.createElement("DIV");
+
+    slider.setAttribute("class", "img-comp-slider");
+
+    img.parentElement.insertBefore(slider, img);
+
+    slider.style.top = (moventSecond / 2) - (slider.offsetHeight / 2) + "px";
+    slider.style.left = (moventFirst / 2) - (slider.offsetWidth / 2) + "px";
+
+    slider.addEventListener("mousedown", slideReady);
+    window.addEventListener("mouseup", slideFinish);
+    slider.addEventListener("touchstart", slideReady);
+    window.addEventListener("touchend", slideFinish);
+
+    function slideReady(aMovent) {
+      aMovent.preventDefault();
+      clicked = 1;
+      window.addEventListener("mousemove", slideMove);
+      window.addEventListener("touchmove", slideMove);
+    }
+
+    function slideFinish() {
+      clicked = 0;
+    }
+
+    function slideMove(aMovent) {
+      var positionMovent;
+      if (clicked == 0) return false;
+
+      positionMovent = getCursorPos(aMovent)
+
+      if (positionMovent < 0) positionMovent = 0;
+      if (positionMovent > moventFirst) positionMovent = moventFirst;
+      slide(positionMovent);
+    }
+
+    function getCursorPos(aMovent) {
+      var after, before = 0;
+      aMovent = (aMovent.changedTouches) ? aMovent.changedTouches[0] : aMovent;
+      after = img.getBoundingClientRect();
+      before = aMovent.pageX - after.left;
+      before = before - window.pageXOffset;
+      return before;
+    }
+
+    function slide(before) {
+      img.style.width = before + "px";
+      slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+    }
+  }
+}
+// Image Comparison Slider
+
+function initComparisonsTow() {
+  var imageTowSliderTow, moventTow;
+  imageTowSliderTow = document.getElementsByClassName("img-comp-overlay-tow");
+  for (moventTow = 0; moventTow < imageTowSliderTow.length; moventTow++) {
+
+    compareImagesTow(imageTowSliderTow[moventTow]);
+  }
+
+  function compareImagesTow(imgSliderTow) {
+
+    var sliderTow, imgSliderTow, clickedTow = 0, moventFirstSliderTow, moventSecondSliderTow;
+
+    moventFirstSliderTow = imgSliderTow.offsetWidth;
+    moventSecondSliderTow = imgSliderTow.offsetHeight;
+    imgSliderTow.style.width = (moventFirstSliderTow / 4) + "px";
+
+    sliderTow = document.createElement("DIV");
+
+    sliderTow.setAttribute("class", "img-comp-slider-tow");
+
+    imgSliderTow.parentElement.insertBefore(sliderTow, imgSliderTow);
+
+    sliderTow.style.top = (moventSecondSliderTow / 4) - (sliderTow.offsetHeight / 4) + "px";
+    sliderTow.style.left = (moventFirstSliderTow / 4) - (sliderTow.offsetWidth / 4) + "px";
+
+    sliderTow.addEventListener("mousedown", slideReadyTow);
+    window.addEventListener("mouseup", slideFinishTow);
+    sliderTow.addEventListener("touchstart", slideReadyTow);
+    window.addEventListener("touchend", slideFinishTow);
+
+    function slideReadyTow(aMoventTow) {
+      aMoventTow.preventDefault();
+      clickedTow = 1;
+      window.addEventListener("mousemove", slideMoveTow);
+      window.addEventListener("touchmove", slideMoveTow);
+    }
+
+    function slideFinishTow() {
+      clickedTow = 0;
+    }
+
+    function slideMoveTow(aMoventTow) {
+      var positionMovent;
+      if (clickedTow == 0) return false;
+
+      positionMovent = getCursorPosTow(aMoventTow)
+
+      if (positionMovent < 0) positionMovent = 0;
+      if (positionMovent > moventFirstSliderTow) positionMovent = moventFirstSliderTow;
+      slideTow(positionMovent);
+    }
+
+    function getCursorPosTow(aMoventTow) {
+      var afterTow, beforeTow = 0;
+      aMoventTow = (aMoventTow.changedTouchesTow) ? aMoventTow.changedTouchesTow[0] : aMoventTow;
+      afterTow = imgSliderTow.getBoundingClientRect();
+      beforeTow = aMoventTow.pageX - afterTow.left;
+      beforeTow = beforeTow - window.pageXOffset;
+      return beforeTow;
+    }
+
+    function slideTow(beforeTow) {
+      imgSliderTow.style.width = beforeTow + "px";
+      sliderTow.style.left = imgSliderTow.offsetWidth - (sliderTow.offsetWidth / 4) + "px";
+    }
+  }
+}
